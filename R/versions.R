793103226d9241ba9852aad94173ee6d0326e426
Tim Norton <6webdesign@gmail.com>
2016-04-07 07:39:58 -0600

    Create versions.R

---
+++ R/versions.R
@@ -0,0 +1,3 @@
+# changed message to '#IMPORTANT NOTE: See the quick start guide by typing ?machina' from '# Get help by typing ?machina'
+# removed period after <https://machi.na/> in description
+# capitalized Machina in the package name since it's our company name
